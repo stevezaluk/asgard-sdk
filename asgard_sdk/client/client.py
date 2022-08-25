20438611717a9b3705c19cc69f869ccc40c6458f
Steven Zaluk <stevezaluk@gmail.com>
2022-08-25 01:24:10 -0400

    Client side functionality

---
+++ asgard_sdk/client/client.py
@@ -0,0 +1,164 @@
+from ..models.config import ClientConfig
+from ..format.print import print_error
+from ..models import generate_object
+
+from json import loads
+from requests import Session
+
+class FailedToFindResource(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+class InvalidContentType(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+    
+class InternalServerError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+class AsgardClient(object):
+    def __init__(self, config: ClientConfig):
+        self.config = config
+        self.config.validate_structure()
+
+        self._session = Session()
+
+        self.base_url = "http://{ri}:{rp}/api/v1".format(ri=self.config.rest_ip, rp=self.config.rest_port)
+        self.anayltics_url = "http://{ri}:{rp}/api/analytics".format(ri=self.config.rest_ip, rp=self.config.rest_port)
+
+        self._cache_info()
+
+    def _query(self, endpoint, params=None, content_type="application/json", analytics=False):
+        if analytics:
+            url = self.anayltics_url + "/" + endpoint
+        else:
+            url = self.base_url + "/" + endpoint
+        
+        resp = self._session.get(url, params=params)
+        
+        if resp.status_code == 404:
+            raise FailedToFindResource("Failed to find resource: {}".format(url))
+
+        if resp.status_code == 500:
+            raise InternalServerError("Internal server error")
+        
+        if resp.headers.get("Content-Type") != content_type:
+            raise InvalidContentType("Content type != {}".format(content_type))
+
+        if content_type == "application/json":
+            resp = loads(resp.content)
+
+        return resp
+
+    def _cache_info(self):
+        resp = self._query("version", analytics=True)
+
+        self.config.cached_info = resp
+
+    def get_sections(self, key=None, limit=15, sort=None, to_dict=False):
+        resp = self._query("sections")
+
+        sections = resp.get("sections")
+
+        if to_dict:
+            return sections
+
+        ret = []
+
+        for section in sections:
+            section = generate_object(section)
+            ret.append(section)
+
+        return ret
+
+
+    def get_section(self, section_name: str, key=None, to_dict=False):
+        params = {}
+
+        if key:
+            params.update({"key":key})
+        
+        url = "sections/" + section_name
+        section = self._query(url, params=params)
+
+        if to_dict:
+            return section
+
+        return generate_object(section)
+
+    def get_file(self, term: str, section_name=None, key=None, plex=False, to_dict=False):
+        params = {}
+        if key:
+            params.update({"key":key})
+
+        if plex:
+            params.update({"plex":True})
+
+        url = "file/"
+        if section_name:
+            url = url + section_name + "/"
+
+        file = self._query(url + term, params=params)
+
+        if to_dict:
+            return file
+
+        return generate_object(file)
+
+    def index(self, section_name=None, key=None, limit=15, sort=None, to_dict=False):
+        params = {}
+
+        if key:
+            params.update({"key":key})
+
+        if limit:
+            params.update({"limit":limit})
+        
+        url = "index"
+        if section_name:
+            url = url + ("/" + section_name)
+        
+        resp = self._query(url, params=params)
+
+        index = resp.get("index")
+
+        if to_dict:
+            return index
+
+        ret = []
+
+        for file in index:
+            file = generate_object(file)
+            ret.append(file)
+
+        return ret
+
+
+    def search(self, file_name: str, section_name=None, key=None, limit=15, sort=None, to_dict=False):
+        params = {"q":file_name}
+
+        if key:
+            params.update({"key":key})
+
+        if limit:
+            params.update({"limit":limit})
+
+        url = "search/"
+        if section_name:
+            url = url + section_name
+
+        resp = self._query(url, params=params)
+
+        search = resp.get("search")
+
+        if to_dict:
+            return search
+
+        ret = []
+
+        for file in search:
+            file = generate_object(file)
+            ret.append(file)
+
+        return ret
