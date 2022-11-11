f1683038ec6733614595dfd28ed68892b594b0c2
Steven Zaluk <stevezaluk@gmail.com>
2022-11-11 12:40:11 -0500

    Fixed dependency issue with bson

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setuptools.setup(name="asgard-sdk",
                 version="1.0", 
                 description="A SDK for asgard",
                 url="https://github.com/stevezaluk/asgard-sdk",
-                install_requires=["pymongo", "plexapi", "colorama", "requests", "bson"],
+                install_requires=["pymongo", "plexapi", "colorama", "requests"],
                 python_requires='>=3')
